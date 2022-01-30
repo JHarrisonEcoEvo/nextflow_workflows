006b23aa5bb87ba739cf7575b3adea60d789c7bd
Josh Harrison <jgharrison1982@gmail.com>
2022-01-30 16:16:10 -0700

    minor tweaks to how much is held in mem

--- bin/split_by_header_nofolder.py
+++ bin/split_by_header_nofolder.py
@@ -21,15 +21,23 @@ if fp.endswith('.gz'):
 else:
     f = open(fp)
 
-record = []
 records = defaultdict(list)
 for line in f:
     name = line[1:].split()[0] #modified so that this splits on white space
     records[name].append(line)
     records[name].append(f.next()) #This used to be readline()
+    #If you get an error here like this: AttributeError: '_io.TextIOWrapper' object has no attribute 'next'
+    #it is because you are using python3. Python really sucks sometimes.
     records[name].append(f.next())
     records[name].append(f.next())
-    if len(records[name]) > 100000:
+
+    #This section was in here so that not too much was stored in memory at a time.
+    #I am not 100% sure what ratio of write often/hold in memory is most performant,
+    # but I suspect that storing as much
+    #in memory as possible and then only writing to disk a few times will be
+    #faster then writing more often. So, I am bumping up the limits here
+
+    if len(records[name]) > 1000000:
         # print "Writing {} lines".format(len(records[name]))
         with open("{}.fastq".format(name), 'a') as fo:
             fo.write(''.join(records[name]))
@@ -37,6 +45,8 @@ for line in f:
 
 f.close()
 
+# the a is for append or make new file. The "with" option automatically closes
+# the file after writing.
 for name in records.keys():
     with open("{}.fastq".format(name), 'a') as fo:
             fo.write(''.join(records[name]))
